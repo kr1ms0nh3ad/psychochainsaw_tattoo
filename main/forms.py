a40da0866b2c3c9a0411191799140e970e00dece
kr1ms0nh3ad <bigsmoke31453@gmail.com>
2026-02-18 11:23:40 +0300

    еще одна заготовка

--- main/forms.py
+++ main/forms.py
@@ -1,1 +1,10 @@
 from django import forms
+from .models import Appointment, Client, Service, Master
+from django.utils import timezone
+
+class AppointmentForm(forms.Form):
+    client_name = forms.CharField(
+        label='ваше имя',
+        max_length=100,
+        widget=forms.TextInput()
+    )
